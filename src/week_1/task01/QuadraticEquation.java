af63efc7e5a972035fd55dcfa48bff7d5b494c0f
AidarValshin <Aidar_V2000@mail.ru>
2021-04-09 22:11:54 +0300

    Done #1

---
+++ src/week_1/task01/QuadraticEquation.java
@@ -0,0 +1,57 @@
+package week_1.task01;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class QuadraticEquation {
+    private final double a;
+    private final double b;
+    private final double c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Уравнение вида: a*x^2 + b*x + c = 0");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Введите a: ");
+            double a = sc.nextDouble();
+            System.out.println();
+            System.out.print("Введите b: ");
+            double b = sc.nextDouble();
+            System.out.println();
+            System.out.print("Введите c: ");
+            double c = sc.nextDouble();
+            System.out.println();
+            System.out.print("Результаты: ");
+            System.out.println(Arrays.toString(new QuadraticEquation(a, b, c).solve()));
+
+        }
+    }
+
+    public double[] solve() {
+        double discriminant = Discriminant.find(a, b, c);
+        if (discriminant > 0) {
+            double[] result = new double[2];
+            result[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+            result[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
+            return result;
+        } else if (discriminant == 0) {
+            return new double[]{(-b) / (2 * a)};
+        }
+        return null;
+    }
+
+    private static class Discriminant {
+        public static double find(double a, double b, double c) {
+            return b * b - 4 * a * c;
+        }
+
+    }
+}
+
